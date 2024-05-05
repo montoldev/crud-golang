f7e3ff25660df8a3a67f8ad06f262fe1a2d0c35c
Montol.cla <montol.cla@truedigital.com>
2024-05-05 22:32:57 +0700

    feat: clean code and edit postman

--- customers/handler.go
+++ customers/handler.go
@@ -2,16 +2,10 @@ package customers
 
 import (
 	"crud-golang/database"
-	"net/http"
 )
 
 type Customers struct {
 	Database database.IDatabase
-}
-
-//go:generate mockgen -source=./handler.go -destination=./mocks/IHandler.go
-type ICustomers interface {
-	CreateCustomer(w http.ResponseWriter, r *http.Request) error
 }
 
 func NewICustomer(db database.IDatabase) *Customers {
